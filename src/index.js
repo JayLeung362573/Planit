85815abb4a178288b6a002a883a397cfcc9914cb
Jiawei Liang <jla1028@sfu.ca>
2025-07-23 15:28:27 -0700

    reformat files stage 1

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './assets/css/styles_home.css';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './assets/css/styles_home.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
